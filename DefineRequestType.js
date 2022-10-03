70445be3075e7051f409a7c52a122bc6b80b67fa
Your Name <you@example.com>
2022-10-03 09:35:44 -0400

    promise all methd

--- DefineRequestType.js
+++ DefineRequestType.js
@@ -24,3 +24,27 @@ function requestType(name, handler) {
 //VALUE returned by the handler function
 // we wrapped promise by try catch block so
 // the exception raised is DIRECTLY given to the callback funtion
+
+//function that sends ping request to each of the nest to see
+//which ones come back
+requestType("ping", () => "pong");
+
+//function using Promise.all methd
+//It returns an array of values produced by the promise.
+//NOte if any of those promises is rejected, the result of
+//Promise.all is also rejected.
+function availableNeighbors(nest) {
+  //requests is holding the array of result (both fals and true)
+  // returned from request methd.
+  let requests = nest.neighbors.map((neighbor) => {
+    return request(nest, neighbor, "ping").then(
+      () => true,
+      () => false
+    );
+  });
+  return Promise.all(requests).then((result) => {
+    nest.neighbors.filter((_, i) => result[i]);
+  });
+}
+// NOTE: filter is used to remove those elements
+//from the neighbors array whose corresponding value is false
