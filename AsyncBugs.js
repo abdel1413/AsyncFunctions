3c480b80c24dbe13907f7669915eddfb12bec1cf
Your Name <you@example.com>
2022-10-05 14:08:19 -0400

    bugs

--- AsyncBugs.js
+++ AsyncBugs.js
@@ -28,4 +28,29 @@ async function chicks(nest, year) {
   return list;
 }
 
-chicks(bigOak, 2022).then(console.log); // won't work bcz of +=
+chicks(bigOak, 2022).then(console.log);
+// won't work bcz of +=
+/**
+ * The problem lies in the += operator, which takes the
+ *  current value of list at the time where the statement 
+ * starts executing and then, when the await finishes, 
+ * sets the list binding to be that value plus the added string.
+
+But between the time where the statement starts 
+executing and the time where it finishes there’s an 
+asynchronous gap. The map expression runs before anything has 
+been added to the list, so each of the += operators starts from 
+an empty string and ends up, when its storage retrieval 
+finishes, setting list to a single-line list—the result of 
+adding its line to the empty string.
+ */
+
+//To solve this issue, call 'join' on the Promise.all instead of
+//building up the list
+
+async function chicks(nest, name) {
+  let lines = network(nest).map(async (name) => {
+    return name + " : " + (await anyStorage(nest, name, `chicks in ${year}`));
+  });
+  return (await Promise.all(lines)).join("\n");
+}
