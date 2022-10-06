1e36abcf5ce1b04ad5d8aad88dd7ddf4bc32e119
Your Name <you@example.com>
2022-10-06 09:33:51 -0400

    async funtion to output the name of scalper

--- AsyncExo.js
+++ AsyncExo.js
@@ -1,8 +1,22 @@
-async function localScalpel(nest) {
+async function locateScalpel(nest) {
   let current = nest.name;
   for (;;) {
     let next = await anyStorage(nest, current, "scalpel");
     if (next == current) return current;
     current = next;
   }
+}
+
+//using same function without async
+// this require another  rerursive function inside and
+function locateScalpel2(nest) {
+  function loop(current) {
+    return anyStorage(nest, current, "scalpel").then((next) => {
+      if (next == current) return current;
+      else {
+        return loop(next);
+      }
+    });
+  }
+  return loop(nest.name);
 }
